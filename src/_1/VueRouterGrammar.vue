3a737478698f2fd7512c32a550adebaefe219715
김수환 <zzanggoon8@hiconsy.com>
2022-10-06 09:37:24 +0900

    yarn setting(bootstrap&quasar&vuex&vueouter)

--- src/_1/VueRouterGrammar.vue
+++ src/_1/VueRouterGrammar.vue
@@ -1,15 +1,8 @@
 <!--
 	1th, 뷰 라우터
-		TODO 1) 라우트를 여러 개 만들 고 싶을 때(src/router/router.jsd에서 '설명')
-		TODO 2) 페이지 이동 링크를 만들고 싶을 때(실무에서)
-			-> to="" 안에 마음대로 경로설정이 가능함.
-			<template v-slot:body-cell-taName="props">
-					<q-td :props="props">
-						<router-link :to="`/ta/info/${props.row.taSerialNo}`" class="text-black">
-							{{ props.value }}
-						</router-link>
-					</q-td>
-			</template>
+		TODO 1) 라우트를 여러 개 만들 고 싶을 때(src/router/router.js에서 '설명')
+		TODO 2) 페이지 이동 링크를 만들고 싶을 때(src/router/router.js에서 '설명')
+		TODO 3) 글 목록 컴포넌트 페이지 만들기(src/component/
 -->
 <template>
 	<div>
---
+++ src/_1/components/PostListComponent.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PostListComponent',
+	props: {
+
+	},
+}
+</script>
+
+<style>
+
+</style>
+
+<!-- TODO 컴포넌트 만들기 -->
+	<!--<template>-->
+	<!--	<div>-->
+
+	<!--	</div>-->
+	<!--</template>-->
+
+	<!--<script>-->
+	<!--export default {-->
+	<!--	name: 'PostListComponent',-->
+	<!--	props: {-->
+
+	<!--	},-->
+	<!--}-->
+	<!--</script>-->
+
+	<!--<style>-->
+
+	<!--</style>-->
