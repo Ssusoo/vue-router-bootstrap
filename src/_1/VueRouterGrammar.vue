678b07e21d721ef2fe876352dc89dc7bf0a20c5d
김수환 <zzanggoon8@hiconsy.com>
2022-10-07 19:20:07 +0900

    1th, 뷰 라우터

--- src/_1/VueRouterGrammar.vue
+++ src/_1/VueRouterGrammar.vue
@@ -1,5 +1,8 @@
 <!--
 	1th, 뷰 라우터
+		TODO Grammar 설명
+			뷰-라우터 설치 및 라우트 라우터 설명(VueRouterBasicGrammar)
+			뷰 라우터 기본법(라우트 path 설정 및 template 태그에 페이지 이동 링크 설정 / router)
 		TODO 1) 라우트를 여러 개 만들 고 싶을 때(src/router/router.js에서 '설명')
 		TODO 2) 페이지 이동 링크를 만들고 싶을 때(src/router/router.js에서 '설명')
 		TODO 3) 글 목록 컴포넌트 페이지 만들기(src/component/
